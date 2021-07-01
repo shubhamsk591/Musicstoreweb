7620826ef56dc4f59682bd7e3d5703e764f14e1b
shubhamsk591 <86595056+shubhamsk591@users.noreply.github.com>
2021-07-01 13:05:10 +0530

    Improve home.html,script1.js

--- script1.js
+++ script1.js
@@ -11,28 +11,90 @@ p1.onclick = function() {
   p1.src="images/play.png";}
 }
 
- s1 =document.getElementById("s2");
-p1 = document.getElementById("play2");
+var s2 =document.getElementById("s2");
+var p2 = document.getElementById("play2");
 
-p1.onclick = function1 () {
-  if( s1.paused){
-    s1.play();
-    p1.src="images/pause.png";
+p2.onclick = function() {
+  if( s2.paused){
+    s2.play();
+    p2.src="images/pause.png";
   }
   else{
-  s1.pause();
-  p1.src="images/play.png";}
+  s2.pause();
+  p2.src="images/play.png";}
 }
 
-var s1 =document.getElementById("s3");
-var p1 = document.getElementById("play3");
+var s3 =document.getElementById("s3");
+var p3 = document.getElementById("play3");
 
-p1.onclick = function2() {
-  if( s1.paused){
-    s1.play();
-    p1.src="images/pause.png";
+p3.onclick = function() {
+  if( s3.paused){
+    s3.play();
+    p3.src="images/pause.png";
   }
   else{
-  s1.pause();
-  p1.src="images/play.png";}
+  s3.pause();
+  p3.src="images/play.png";}
+}
+var s4 =document.getElementById("s4");
+var p4 = document.getElementById("play4");
+
+p4.onclick = function() {
+  if( s4.paused){
+    s4.play();
+    p4.src="images/pause.png";
+  }
+  else{
+  s4.pause();
+  p4.src="images/play.png";}
+}
+var s5 =document.getElementById("s5");
+var p5 = document.getElementById("play5");
+
+p5.onclick = function() {
+  if( s5.paused){
+    s5.play();
+    p5.src="images/pause.png";
+  }
+  else{
+  s5.pause();
+  p5.src="images/play.png";}
+}
+
+var s6 =document.getElementById("s6");
+var p6 = document.getElementById("play6");
+
+p6.onclick = function() {
+  if( s6.paused){
+    s6.play();
+    p6.src="images/pause.png";
+  }
+  else{
+  s6.pause();
+  p6.src="images/play.png";}
+}
+
+var s7 =document.getElementById("s7");
+var p7 = document.getElementById("play7");
+
+p7.onclick = function() {
+  if( s7.paused){
+    s7.play();
+    p7.src="images/pause.png";
+  }
+  else{
+  s7.pause();
+  p7.src="images/play.png";}
+}
+var s8=document.getElementById("s8");
+var p8 = document.getElementById("play8");
+
+p8.onclick = function() {
+  if( s8.paused){
+    s8.play();
+    p8.src="images/pause.png";
+  }
+  else{
+  s8.pause();
+  p8.src="images/play.png";}
 }
